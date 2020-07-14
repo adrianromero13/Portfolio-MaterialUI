7f31a889715a1873658185c96d07c16fa7981944
Adrian Romero <radrian031613@gmail.com>
2020-07-13 18:50:36 -0700

    set up server.js for use of mongoose and express

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// const routes = require('./routes');
 
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// set up middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+// use of routes
+// app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/portfolioWork',
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  });
+
+app.listen(PORT);

03f9029221435f879a7bc7bdf19ec7728189f2c6
Adrian Romero <radrian031613@gmail.com>
2020-07-13 22:20:16 -0700

    added new upload image controller function.

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,13 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
 const { Project, Image } = require('../models');
-// set up model variable
 
 module.exports = {
   getProjects: async (req, res) => {
     try {
-      const project = await Project.find();
-      return res.status(200).json(project);
+      const projects = await Project.find();
+      return res.status(200).json(projects);
     } catch (error) {
       return res.status(403).json({ error });
     }
   },
+
+  uploadImage: async (req, res, next) => {
+    try {
+    const obj = {
+      name: req.body.name,
+      description: req.body.description,
+      img: {
+        data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+        contentType: 'image/png'
+      }
+    }
+      const newImage = await Image.create(obj);
+      return res.redirect('/')
+    } catch (error) {
+      
+    }
+  }
 };
